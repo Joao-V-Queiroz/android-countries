f36be09999acc9b7a7d304c211911d8d05509203
Mahram Z. Foadi <mahramf@airg.com>
2017-02-15 17:45:58 -0800

    adding flags

---
+++ README.md
@@ -0,0 +1,5 @@
+# Android Countries
+Coming soon.
+
+## Contributions
+Please refer to the [contribution instructions](https://airg.github.io/#contribute).
---
+++ countries/src/main/java/com/airg/android/Country.java
@@ -0,0 +1,301 @@
+package com.airg.android;
+
+import android.support.annotation.DrawableRes;
+
+import com.airg.android.countries.R;
+
+/**
+ * A list of countries, their latin and localized names, their international prefix codes, and alpha2 and alpha3 codes.
+ *
+ * @author Mahram Z. Foadi
+ */
+@SuppressWarnings({"unused", "SpellCheckingInspection"})
+public enum Country {
+    AF("AFG", "Afghanistan", "\u202Bافغانستان\u202C\u200E", 93, R.drawable.ic_flag_af),
+    AX("ALA", "Åland Islands", null, 358, R.drawable.ic_flag_ax),
+    AL("ALB", "Albania", "Shqipëri", 355, R.drawable.ic_flag_al),
+    DZ("DZA", "Algeria", "\u202Bالجزائر\u202C\u200E", 213, R.drawable.ic_flag_dz),
+    AS("ASM", "American Samoa", null, 1684, R.drawable.ic_flag_as),
+    AD("AND", "Andorra", null, 376, R.drawable.ic_flag_ad),
+    AO("AGO", "Angola", null, 244, R.drawable.ic_flag_ao),
+    AI("AIA", "Anguilla", null, 1264, R.drawable.ic_flag_ai),
+    AG("ATG", "Antigua and Barbuda", null, 1268, R.drawable.ic_flag_ag),
+    AR("G", "Argentina", null, 54, R.drawable.ic_flag_ar),
+    AM("ARM", "Armenia", "Հայաստան", 374, R.drawable.ic_flag_am),
+    AW("ABW", "Aruba", null, 297, R.drawable.ic_flag_aw),
+    AU("AUS", "Australia", null, 61, R.drawable.ic_flag_au),
+    AT("AUT", "Austria", "Österreich", 43, R.drawable.ic_flag_at),
+    AZ("AZE", "Azerbaijan", "Azərbaycan", 994, R.drawable.ic_flag_az),
+    BS("BHS", "Bahamas", null, 1242, R.drawable.ic_flag_bs),
+    BH("BHR", "Bahrain", "\u202Bالبحرين\u202C\u200E", 973, R.drawable.ic_flag_bh),
+    BD("BGD", "Bangladesh", "বাংলাদেশ", 880, R.drawable.ic_flag_bd),
+    BB("BRB", "Barbados", null, 1246, R.drawable.ic_flag_bb),
+    BY("BLR", "Belarus", "Беларусь", 375, R.drawable.ic_flag_by),
+    BE("BEL", "Belgium", "België", 32, R.drawable.ic_flag_be),
+    BZ("BLZ", "Belize", null, 501, R.drawable.ic_flag_bz),
+    BJ("BEN", "Benin", "Bénin", 229, R.drawable.ic_flag_bj),
+    BM("BMU", "Bermuda", null, 1441, R.drawable.ic_flag_bm),
+    BT("BTN", "Bhutan", "འབྲུག", 975, R.drawable.ic_flag_bt),
+    BO("BOL", "Bolivia", null, 591, R.drawable.ic_flag_bo),
+    BA("BIH", "Bosnia and Herzegovina", "Босна и Херцеговина", 387, R.drawable.ic_flag_ba),
+    BW("BWA", "Botswana", null, 267, R.drawable.ic_flag_bw),
+    BR("BRA", "Brazil", "Brasil", 55, R.drawable.ic_flag_br),
+    IO("IOT", "British Indian Ocean Territory", null, 246, R.drawable.ic_flag_io),
+    VG("VGB", "British Virgin Islands", null, 1284, R.drawable.ic_flag_vg),
+    BN("BRN", "Brunei", null, 673, R.drawable.ic_flag_bn),
+    BG("BGR", "Bulgaria", "България", 359, R.drawable.ic_flag_bg),
+    BF("BFA", "Burkina Faso", null, 226, R.drawable.ic_flag_bf),
+    BI("BDI", "Burundi", "Uburundi", 257, R.drawable.ic_flag_bi),
+    KH("KHM", "Cambodia", "កម្ពុជា", 855, R.drawable.ic_flag_kh),
+    CM("CMR", "Cameroon", "Cameroun", 237, R.drawable.ic_flag_cm),
+    CA("CAN", "Canada", null, 1, R.drawable.ic_flag_ca),
+    CV("CPV", "Cape Verde", "Kabu Verdi", 238, R.drawable.ic_flag_cv),
+    BQ("BES", "Caribbean Netherlands", null, 599, R.drawable.ic_flag_nl),
+    KY("CYM", "Cayman Islands", null, 1345, R.drawable.ic_flag_ky),
+    CF("CAF", "Central African Republic", "République centrafricaine", 236, R.drawable.ic_flag_cf),
+    TD("TCD", "Chad", "Tchad", 235, R.drawable.ic_flag_td),
+    CL("CHL", "Chile", null, 56, R.drawable.ic_flag_cl),
+    CN("CHN", "China", "中国", 86, R.drawable.ic_flag_cn),
+    CX("CXR", "Christmas Island", null, 61, R.drawable.ic_flag_cx),
+    CC("CCK", "Cocos (Keeling) Islands", null, 61, R.drawable.ic_flag_cc),
+    CO("COL", "Colombia", null, 57, R.drawable.ic_flag_co),
+    KM("COM", "Comoros", "\u202Bجزر القمر\u202C\u200E", 269, R.drawable.ic_flag_km),
+    CD("COD", "Congo (DRC)", "Jamhuri ya Kidemokrasia ya Kongo", 234, R.drawable.ic_flag_cd),
+    CG("COG", "Congo (Republic)", "Congo-Brazzaville", 242, R.drawable.ic_flag_cg),
+    CK("COK", "Cook Islands", null, 682, R.drawable.ic_flag_ck),
+    CR("CRI", "Costa Rica", null, 506, R.drawable.ic_flag_cr),
+    CI("CIV", "Cote d'Ivoire", "Côte d’Ivoire", 225, R.drawable.ic_flag_ci),
+    HR("HRV", "Croatia", "Hrvatska", 385, R.drawable.ic_flag_hr),
+    CU("CUB", "Cuba", null, 53, R.drawable.ic_flag_cu),
+    CW("CUW", "Curacao", "Curaçao", 599, R.drawable.ic_flag_cw),
+    CY("CYP", "Cyprus", "Κύπρος", 357, R.drawable.ic_flag_cy),
+    CZ("CZE", "Czech Republic", "Česká republika", 420, R.drawable.ic_flag_cz),
+    DK("DNK", "Denmark", "Danmark", 45, R.drawable.ic_flag_dk),
+    DJ("DJI", "Djibouti", null, 253, R.drawable.ic_flag_dj),
+    DM("DMA", "Dominica", null, 1767, R.drawable.ic_flag_dm),
+    DO("DOM", "Dominican Republic", "República Dominicana", 1, R.drawable.ic_flag_do),
+    EC("ECU", "Ecuador", null, 593, R.drawable.ic_flag_ec),
+    EG("EGY", "Egypt", "\u202Bمصر\u202C\u200E", 20, R.drawable.ic_flag_eg),
+    SV("SLV", "El Salvador", null, 503, R.drawable.ic_flag_sv),
+    GQ("GNQ", "Equatorial Guinea", "Guinea Ecuatorial", 240, R.drawable.ic_flag_gq),
+    ER("ERI", "Eritrea", null, 291, R.drawable.ic_flag_er),
+    EE("EST", "Estonia", "Eesti", 372, R.drawable.ic_flag_ee),
+    ET("ETH", "Ethiopia", null, 251, R.drawable.ic_flag_et),
+    FK("FLK", "Falkland Islands", "Islas Malvinas", 500, R.drawable.ic_flag_fk),
+    FO("FRO", "Faroe Islands", "Føroyar", 298, R.drawable.ic_flag_fo),
+    FJ("FJI", "Fiji", null, 679, R.drawable.ic_flag_fj),
+    FI("FIN", "Finland", "Suomi", 358, R.drawable.ic_flag_fi),
+    FR("FRA", "France", null, 33, R.drawable.ic_flag_fr),
+    GF("GUF", "French Guiana", "Guyane française", 594, R.drawable.ic_flag_gf),
+    PF("PYF", "French Polynesia", "Polynésie française", 689, R.drawable.ic_flag_pf),
+    GA("GAB", "Gabon", null, 241, R.drawable.ic_flag_ga),
+    GM("GMB", "Gambia", null, 220, R.drawable.ic_flag_gm),
+    GE("GEO", "Georgia", "საქართველო", 995, R.drawable.ic_flag_ge),
+    DE("DEU", "Germany", "Deutschland", 49, R.drawable.ic_flag_de),
+    GH("GHA", "Ghana", "Gaana", 233, R.drawable.ic_flag_gh),
+    GI("GIB", "Gibraltar", null, 350, R.drawable.ic_flag_gi),
+    GR("GRC", "Greece", "Ελλάδα", 30, R.drawable.ic_flag_gr),
+    GL("GRL", "Greenland", "Kalaallit Nunaat", 299, R.drawable.ic_flag_gl),
+    GD("GRD", "Grenada", null, 1473, R.drawable.ic_flag_gd),
+    GP("GLP", "Guadeloupe", null, 590, R.drawable.ic_flag_fr),
+    GU("GUM", "Guam", null, 1671, R.drawable.ic_flag_gu),
+    GT("GTM", "Guatemala", null, 502, R.drawable.ic_flag_gt),
+    GG("GGY", "Guernsey", null, 44, R.drawable.ic_flag_gg),
+    GN("GIN", "Guinea", "Guinée", 224, R.drawable.ic_flag_gn),
+    GW("GNB", "Guinea-Bissau", "Guiné Bissau", 245, R.drawable.ic_flag_gw),
+    GY("GUY", "Guyana", null, 592, R.drawable.ic_flag_gy),
+    HT("HTI", "Haiti", null, 509, R.drawable.ic_flag_ht),
+    HN("HND", "Honduras", null, 504, R.drawable.ic_flag_hn),
+    HK("HKG", "Hong Kong", "香港", 852, R.drawable.ic_flag_hk),
+    HU("HUN", "Hungary", "Magyarország", 36, R.drawable.ic_flag_hu),
+    IS("ISL", "Iceland", "Ísland", 354, R.drawable.ic_flag_is),
+    IN("IND", "India", "भारत", 91, R.drawable.ic_flag_in),
+    ID("IDN", "Indonesia", null, 62, R.drawable.ic_flag_id),
+    IR("IRN", "\u202Bایران\u202C\u200E", null, 98, R.drawable.ic_flag_ir),
+    IQ("IRQ", "Iraq", "\u202Bالعراق\u202C\u200E", 964, R.drawable.ic_flag_unknown),
+    IE("IRL", "Ireland", null, 353, R.drawable.ic_flag_unknown),
+    IM("IMN", "Isle of Man", null, 44, R.drawable.ic_flag_unknown),
+    IL("ISR", "Israel", "\u202Bישראל\u202C\u200E", 972, R.drawable.ic_flag_unknown),
+    IT("ITA", "Italy", "Italia", 39, R.drawable.ic_flag_unknown),
+    JM("JAM", "Jamaica", null, 1876, R.drawable.ic_flag_unknown),
+    JP("JPN", "Japan", "日本", 81, R.drawable.ic_flag_unknown),
+    JE("JEY", "Jersey", null, 44, R.drawable.ic_flag_unknown),
+    JO("JOR", "Jordan", "\u202Bالأردن\u202C\u200E", 962, R.drawable.ic_flag_unknown),
+    KZ("KAZ", "Kazakhstan", "Казахстан", 7, R.drawable.ic_flag_unknown),
+    KE("KEN", "Kenya", null, 254, R.drawable.ic_flag_unknown),
+    KI("KIR", "Kiribati", null, 686, R.drawable.ic_flag_unknown),
+    XK("XXK", "Kosovo", null, 383, R.drawable.ic_flag_unknown),
+    KW("KWT", "Kuwait", "\u202Bالكويت\u202C\u200E", 965, R.drawable.ic_flag_unknown),
+    KG("KGZ", "Kyrgyzstan", "Кыргызстан", 996, R.drawable.ic_flag_unknown),
+    LA("LAO", "Laos", "ລາວ", 856, R.drawable.ic_flag_unknown),
+    LV("LVA", "Latvia ", "Latvija", 371, R.drawable.ic_flag_unknown),
+    LB("LBN", "Lebanon", "\u202Bلبنان\u202C\u200E", 961, R.drawable.ic_flag_unknown),
+    LS("LSO", "Lesotho", null, 266, R.drawable.ic_flag_unknown),
+    LR("LBR", "Liberia", null, 231, R.drawable.ic_flag_unknown),
+    LY("LBY", "Libya", "\u202Bليبيا\u202C\u200E", 218, R.drawable.ic_flag_unknown),
+    LI("LIE", "Liechtenstein", null, 423, R.drawable.ic_flag_unknown),
+    LT("LTU", "Lithuania", "Lietuva", 370, R.drawable.ic_flag_unknown),
+    LU("LUX", "Luxembourg", null, 352, R.drawable.ic_flag_unknown),
+    MO("MAC", "Macau", "澳門", 853, R.drawable.ic_flag_unknown),
+    MK("MKD", "Macedonia", "Македонија", 389, R.drawable.ic_flag_unknown),
+    MG("MDG", "Madagascar", "Madagasikara", 261, R.drawable.ic_flag_unknown),
+    MW("MWI", "Malawi", null, 265, R.drawable.ic_flag_unknown),
+    MY("MYS", "Malaysia", null, 60, R.drawable.ic_flag_unknown),
+    MV("MDV", "Maldives", null, 960, R.drawable.ic_flag_unknown),
+    ML("MLI", "Mali", null, 223, R.drawable.ic_flag_unknown),
+    MT("MLT", "Malta", null, 356, R.drawable.ic_flag_unknown),
+    MH("MHL", "Marshall Islands", null, 692, R.drawable.ic_flag_unknown),
+    MQ("MTQ", "Martinique", null, 596, R.drawable.ic_flag_unknown),
+    MR("MRT", "Mauritania", "\u202Bموريتانيا\u202C\u200E", 222, R.drawable.ic_flag_unknown),
+    MU("MUS", "Mauritius", "Moris", 230, R.drawable.ic_flag_unknown),
+    YT("MYT", "Mayotte", null, 262, R.drawable.ic_flag_unknown),
+    MX("MEX", "Mexico", "México", 52, R.drawable.ic_flag_unknown),
+    FM("FSM", "Micronesia", null, 691, R.drawable.ic_flag_unknown),
+    MD("MDA", "Moldova", "Republica Moldova", 373, R.drawable.ic_flag_unknown),
+    MC("MCO", "Monaco", null, 377, R.drawable.ic_flag_unknown),
+    MN("MNG", "Mongolia", "Монгол", 976, R.drawable.ic_flag_unknown),
+    ME("MNE", "Montenegro", "Crna Gora", 382, R.drawable.ic_flag_unknown),
+    MS("MSR", "Montserrat", null, 1664, R.drawable.ic_flag_unknown),
+    MA("MAR", "Morocco", "\u202Bالمغرب\u202C\u200E", 212, R.drawable.ic_flag_unknown),
+    MZ("MOZ", "Mozambique", "Moçambique", 258, R.drawable.ic_flag_unknown),
+    MM("MMR", "Myanmar", "မြန်မာ", 95, R.drawable.ic_flag_unknown),
+    NA("NAM", "Namibia", "Namibië", 264, R.drawable.ic_flag_unknown),
+    NR("NRU", "Nauru", null, 674, R.drawable.ic_flag_unknown),
+    NP("NPL", "Nepal", "नेपाल", 977, R.drawable.ic_flag_unknown),
+    NL("NLD", "Netherlands", "Nederland", 31, R.drawable.ic_flag_nl),
+    NC("NCL", "New Caledonia", "Nouvelle-Calédonie", 687, R.drawable.ic_flag_unknown),
+    NZ("NZL", "New Zealand", null, 64, R.drawable.ic_flag_unknown),
+    NI("NIC", "Nicaragua", null, 505, R.drawable.ic_flag_unknown),
+    NE("NER", "Niger", "Nijar", 227, R.drawable.ic_flag_unknown),
+    NG("NGA", "Nigeria", null, 234, R.drawable.ic_flag_unknown),
+    NU("NIU", "Niue", null, 683, R.drawable.ic_flag_unknown),
+    NF("NFK", "Norfolk Island", null, 672, R.drawable.ic_flag_unknown),
+    KP("PRK", "North Korea", "조선 민주주의 인민 공화국", 850, R.drawable.ic_flag_unknown),
+    MP("MNP", "Northern Mariana Islands", null, 1670, R.drawable.ic_flag_unknown),
+    NO("NOR", "Norway", "Norge", 47, R.drawable.ic_flag_unknown),
+    OM("OMN", "Oman", "\u202Bعُمان\u202C\u200E", 968, R.drawable.ic_flag_unknown),
+    PK("pak", "Pakistan", "\u202Bپاکستان\u202C\u200E", 92, R.drawable.ic_flag_unknown),
+    PW("PLW", "Palau", null, 680, R.drawable.ic_flag_unknown),
+    PS("PSE", "Palestine", "\u202Bفلسطين\u202C\u200E", 970, R.drawable.ic_flag_unknown),
+    PA("PAN", "Panama", "Panamá", 507, R.drawable.ic_flag_unknown),
+    PG("PNG", "Papua New Guinea", null, 675, R.drawable.ic_flag_unknown),
+    PY("PRY", "Paraguay", null, 595, R.drawable.ic_flag_unknown),
+    PE("PER", "Peru", "Perú", 51, R.drawable.ic_flag_unknown),
+    PH("PHL", "Philippines", null, 63, R.drawable.ic_flag_unknown),
+    PL("POL", "Poland", "Polska", 48, R.drawable.ic_flag_unknown),
+    PT("PRT", "Portugal", null, 351, R.drawable.ic_flag_unknown),
+    PR("PRI", "Puerto Rico", null, 1, R.drawable.ic_flag_unknown),
+    QA("QAT", "Qatar", "\u202Bقطر\u202C\u200E", 974, R.drawable.ic_flag_unknown),
+    RE("REU", "Réunion", "La Réunion", 262, R.drawable.ic_flag_unknown),
+    RO("ROU", "Romania", "România", 40, R.drawable.ic_flag_unknown),
+    RU("RUS", "Russia", "Россия", 7, R.drawable.ic_flag_unknown),
+    RW("RWA", "Rwanda", null, 250, R.drawable.ic_flag_unknown),
+    BL("BLM", "Saint Barthélemy", "Saint-Barthélemy", 590, R.drawable.ic_flag_unknown),
+    SH("SHN", "Saint Helena", null, 290, R.drawable.ic_flag_unknown),
+    KN("KNA", "Saint Kitts and Nevis", null, 1869, R.drawable.ic_flag_unknown),
+    LC("LCA", "Saint Lucia", null, 1758, R.drawable.ic_flag_unknown),
+    MF("MAF", "Saint Martin", "Saint-Martin (partie française)", 590, R.drawable.ic_flag_unknown),
+    PM("SPM", "Saint Pierre and Miquelon", "Saint-Pierre-et-Miquelon", 508, R.drawable.ic_flag_unknown),
+    VC("VCT", "Saint Vincent and the Grenadines", null, 1784, R.drawable.ic_flag_unknown),
+    WS("WSM", "Samoa", null, 685, R.drawable.ic_flag_unknown),
+    SM("SMR", "San Marino", null, 378, R.drawable.ic_flag_unknown),
+    ST("STP", "São Tomé and Príncipe", "São Tomé e Príncipe", 239, R.drawable.ic_flag_unknown),
+    SA("SAU", "Saudi Arabia", "\u202Bالمملكة العربية السعودية\u202C\u200E", 966, R.drawable.ic_flag_unknown),
+    SN("SEN", "Senegal", "Sénégal", 221, R.drawable.ic_flag_unknown),
+    RS("SRB", "Serbia", "Србија", 381, R.drawable.ic_flag_unknown),
+    SC("SYC", "Seychelles", null, 248, R.drawable.ic_flag_unknown),
+    SL("SLE", "Sierra Leone", null, 232, R.drawable.ic_flag_unknown),
+    SG("SGP", "Singapore", null, 65, R.drawable.ic_flag_unknown),
+    SX("SXM", "Sint Maarten", null, 1721, R.drawable.ic_flag_unknown),
+    SK("SVK", "Slovakia", "Slovensko", 421, R.drawable.ic_flag_unknown),
+    SI("SVN", "Slovenia", "Slovenija", 386, R.drawable.ic_flag_unknown),
+    SB("SLB", "Solomon Islands", null, 677, R.drawable.ic_flag_unknown),
+    SO("SOM", "Somalia", "Soomaaliya", 252, R.drawable.ic_flag_unknown),
+    ZA("ZAF", "South Africa", null, 27, R.drawable.ic_flag_unknown),
+    KR("KOR", "South Korea", "대한민국", 82, R.drawable.ic_flag_unknown),
+    SS("SSD", "South Sudan", "\u202Bجنوب السودان\u202C\u200E", 211, R.drawable.ic_flag_unknown),
+    ES("ESP", "Spain", "España", 34, R.drawable.ic_flag_unknown),
+    LK("LKA", "Sri Lanka", "ශ්‍රී ලංකාව", 94, R.drawable.ic_flag_unknown),
+    SD("SDN", "Sudan", "\u202Bالسودان\u202C\u200E", 249, R.drawable.ic_flag_unknown),
+    SR("SUR", "Suriname", null, 597, R.drawable.ic_flag_unknown),
+    SJ("SJM", "Svalbard and Jan Mayen", null, 47, R.drawable.ic_flag_unknown),
+    SZ("SWZ", "Swaziland", null, 268, R.drawable.ic_flag_unknown),
+    SE("SWE", "Sweden", "Sverige", 46, R.drawable.ic_flag_unknown),
+    CH("CHE", "Switzerland", "Schweiz", 41, R.drawable.ic_flag_unknown),
+    SY("SYR", "Syria", "\u202Bسوريا\u202C\u200E", 963, R.drawable.ic_flag_unknown),
+    TW("TWN", "Taiwan", "台灣", 886, R.drawable.ic_flag_unknown),
+    TJ("TJK", "Tajikistan", null, 992, R.drawable.ic_flag_unknown),
+    TZ("TZA", "Tanzania", null, 255, R.drawable.ic_flag_unknown),
+    TH("THA", "Thailand", "ไทย", 66, R.drawable.ic_flag_unknown),
+    TL("TLS", "Timor-Leste", null, 670, R.drawable.ic_flag_unknown),
+    TG("TGO", "Togo", null, 228, R.drawable.ic_flag_unknown),
+    TK("TKL", "Tokelau", null, 690, R.drawable.ic_flag_unknown),
+    TO("TON", "Tonga", null, 676, R.drawable.ic_flag_unknown),
+    TT("TTO", "Trinidad and Tobago", null, 1868, R.drawable.ic_flag_unknown),
+    TN("TUN", "Tunisia", "\u202Bتونس\u202C\u200E", 216, R.drawable.ic_flag_unknown),
+    TR("TUR", "Turkey", "Türkiye", 90, R.drawable.ic_flag_unknown),
+    TM("TKM", "Turkmenistan", null, 993, R.drawable.ic_flag_unknown),
+    TC("TCA", "Turks and Caicos Islands", null, 1649, R.drawable.ic_flag_unknown),
+    TV("TUV", "Tuvalu", null, 688, R.drawable.ic_flag_unknown),
+    VI("VIR", "U.S. Virgin Islands", null, 1340, R.drawable.ic_flag_unknown),
+    UG("UGA", "Uganda", null, 256, R.drawable.ic_flag_unknown),
+    UA("UKR", "Ukraine", "Україна", 380, R.drawable.ic_flag_unknown),
+    AE("ARE", "United Arab Emirates", "\u202Bالإمارات العربية المتحدة\u202C\u200E", 971, R.drawable.ic_flag_unknown),
+    GB("GBR", "United Kingdom", null, 44, R.drawable.ic_flag_unknown),
+    US("USA", "United States", null, 1, R.drawable.ic_flag_unknown),
+    UY("URY", "Uruguay", null, 598, R.drawable.ic_flag_unknown),
+    UZ("UZB", "Uzbekistan", "Oʻzbekiston", 998, R.drawable.ic_flag_unknown),
+    VU("VUT", "Vanuatu", null, 678, R.drawable.ic_flag_unknown),
+    VA("VAT", "Vatican City", "Città del Vaticano", 39, R.drawable.ic_flag_unknown),
+    VE("VEN", "Venezuela", null, 58, R.drawable.ic_flag_unknown),
+    VN("VNM", "Vietnam", "Việt Nam", 84, R.drawable.ic_flag_unknown),
+    WF("WLF", "Wallis and Futuna", null, 681, R.drawable.ic_flag_unknown),
+    EH("ESH", "Western Sahara", "\u202Bالصحراء الغربية\u202C\u200E", 212, R.drawable.ic_flag_unknown),
+    YE("YEM", "Yemen", "\u202Bاليمن\u202C\u200E", 967, R.drawable.ic_flag_unknown),
+    ZM("ZMB", "Zambia", null, 260, R.drawable.ic_flag_unknown),
+    ZW("ZWE", "Zimbabwe", null, 263, R.drawable.ic_flag_unknown)
+    ;
+
+    /**
+     * Country's alpha 3 name (e.g. IRN)
+     */
+    public final String alpha3;
+
+    /**
+     * Country's latin name (e.g. "Iran")
+     */
+    public final String latinName;
+
+    /**
+     * Country's internation phone prefix code
+     */
+    public final int code;
+
+    /**
+     * Country flag drawable resource id
+     */
+    @DrawableRes public final int flag;
+
+    private final String localizedName;
+
+    Country(final String a3, final String en, final String ln, final int intlCode, final @DrawableRes int icFlag) {
+        if (intlCode <= 0) throw new IllegalArgumentException("country code prefix");
+        if (null == a3 || a3.length() != 3) throw new IllegalArgumentException("Invalid Alpha3");
+
+        alpha3 = a3;
+        latinName = en;
+        localizedName = ln;
+        code = intlCode;
+        flag = icFlag;
+    }
+
+    /**
+     * Get the country's localized name (e.g. "ایران")
+     *
+     * @return localized name of the country
+     */
+    public String localizedName() {
+        return null == localizedName ? latinName : localizedName;
+    }
+}
+
